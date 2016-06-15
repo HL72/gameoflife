c01e3eef20e27f63f5bb434828b58288896274ff
olivier <guyetolivier@yahoo.fr>
2016-06-15 16:26:37 +0200

    Afficher

--- src/plugins/jeudelavie/JeuDeLaVie.java
+++ src/plugins/jeudelavie/JeuDeLaVie.java
@@ -1,6 +1,13 @@
 package plugins.jeudelavie;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.util.List;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import descripteur.Descripteur;
 import plateforme.Plateforme;
@@ -12,9 +19,17 @@ import plugins.producteurs.ProducteurImpl;
 public class JeuDeLaVie extends Plugin implements Application {
 	
 	MatriceModele modele;
+	JFrame frame;
+	JPanel container;
 	
 	public JeuDeLaVie(Plateforme p, Descripteur d) throws Exception {
 		super(p,d);
+		frame = new JFrame("Jeu de la Vie");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		container = new JPanel();
+		container.setBackground(Color.white);
+		container.setLayout(null);
+		container.setPreferredSize(new Dimension(400, 400));
 	}
 
 	@Override
@@ -101,6 +116,30 @@ public class JeuDeLaVie extends Plugin implements Application {
 		Descripteur producteurDesc = producteursDesc.get(0);
 		ProducteurImpl producteur = (ProducteurImpl) Plateforme.getPlugin(producteurDesc);
 		modele = producteur.getMatrice();
+	}
+	
+	private void afficher() throws Exception {
+		int width = 10;
+		int height = 10;
+		int[][] m = modele.getMatrice();
+		for(int i = 0 ; i< m.length ; i ++) {
+			for(int j = 0 ; j < m[0].length ; j++){
+				JLabel cell = new JLabel();
+				cell.setOpaque(true);
+				cell.setBounds(i*width, j*height, width, height);
+				cell.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+				if(m[i][j] == 0){
+					cell.setBackground(Color.WHITE);
+				}
+				if(m[i][j] == 1){
+					cell.setBackground(Color.BLACK);
+				}
+				container.add(cell);
+			}
+		}
+		frame.add(container);
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 }
